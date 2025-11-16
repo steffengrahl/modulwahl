5b0350ef09a58d7d4107dc08aac9da186b01f9ae
Steffen Grahl <steffen@sgwebservice.de>
2025-11-16 21:10:30 +0100

    Upgrade to Symfony 4 flex structure

--- app/AppCache.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace app;
-
-use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
-
-class AppCache extends HttpCache {}
--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,8 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
