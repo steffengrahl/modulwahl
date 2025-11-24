f57afdeba9d60103fe2d99c6c1d158c0447cfdad
Steffen Grahl <steffen@sgwebservice.de>
2025-11-24 20:55:39 +0100

    Upgrade to Symfony 5

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
