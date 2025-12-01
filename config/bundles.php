23b3f661af26fb22c47d4f8692298211e3b597af
Steffen <steffen@sgwebservice.de>
2025-12-01 09:02:24 +0100

    Remove doctrine packages because there is no database needed for now

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
